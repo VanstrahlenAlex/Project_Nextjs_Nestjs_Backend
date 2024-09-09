1973deaa70b93f43cdb7780b8aac326d69f1e45d
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-09-08 22:15:07 -0500

    8-09-2024

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { PrismaService } from './prisma/prisma.service';
+
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+// export class CreateProductDto {
+
+// }
+import { Product} from '@prisma/client';
+
+export type CreateProductDto = Omit<Product, 'id' | 'createdAt' | 'updatedAt'>;;
